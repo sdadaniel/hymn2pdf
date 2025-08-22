bc17185e6f566749a3e9d5b437e7d0c9d84a4a72
sdadaniel <sdadaniel0206@hanmail.net>
2025-08-22 14:08:39 +0900

    feat: vercel analytics 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/next';
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -50,7 +51,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        
         {children}
+        <Analytics/>
       </body>
     </html>
   );
