9e19153e6872b74e3e3d72ac70f684c1ec6b5496
sdadaniel <sdadaniel0206@hanmail.net>
2025-08-22 15:15:32 +0900

    feat: debug 모드 추가

--- src/app/modules/ga/GoogleAnalytics.tsx
+++ src/app/modules/ga/GoogleAnalytics.tsx
@@ -1,6 +1,8 @@
 import Script from "next/script";
 
 export default function GoogleAnalytics() {
+  const isDebug = process.env.NEXT_PUBLIC_IS_GA_DEBUG === 'true';
+  
   return (
     <>
       {/* Google tag (gtag.js) */}
@@ -13,7 +15,13 @@ export default function GoogleAnalytics() {
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}');
+          
+          // GA 설정 - 디버그 모드 지원
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}', {
+            debug_mode: ${isDebug},
+            ${isDebug ? 'send_page_view: false,' : ''}
+            ${isDebug ? 'anonymize_ip: true,' : ''}
+          });
         `}
       </Script>
       
@@ -25,6 +33,12 @@ export default function GoogleAnalytics() {
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
           })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');
+          
+          // GTM 디버그 모드 설정
+          ${isDebug ? `
+          console.log('Google Tag Manager Debug Mode Enabled');
+          console.log('GTM ID:', '${process.env.NEXT_PUBLIC_GTM_ID}');
+          ` : ''}
         `}
       </Script>
     </>
